422d4630a83ebf4e077a3df65cf18937beb136fd
Rashan Hodge <rashan.hodge@gmail.com>
2017-10-26 21:32:08 -0400

    Removed the .gitignore

---
+++ assets/javascript/config.js
@@ -0,0 +1,10 @@
+
+
+masterFirebaseConfig = {
+  apiKey: "AIzaSyCVWOtMtoL93jMYfR0CSBksZ-ZYwSV9iCA",
+  authDomain: "trainscheduler-63bdb.firebaseapp.com",
+  databaseURL: "https://trainscheduler-63bdb.firebaseio.com",
+  projectId: "trainscheduler-63bdb",
+  storageBucket: "",
+  messagingSenderId: "684707367885"
+};
